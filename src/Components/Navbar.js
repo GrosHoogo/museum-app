2acbc616acce2e0db1d15a074f61a50013c25318
«GrosHoogo» <hugo.beroujon@outlook.com>
2024-05-24 16:46:46 +0200

    Navbar Change

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,15 +1,16 @@
 import React from 'react';
+import logoImg from '../Assets/museum-logo.png'; // Importez votre image ici
 import '../Styles/Navbar.css';
 
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <div className="logo">Logo</div>
+      <img src={logoImg} alt="Logo" className="logo-img" /> {/* Utilisez la balise img avec votre image */}
       <ul className="nav-links">
         <li><a href="#">Accueil</a></li>
         <li><a href="#">À propos</a></li>
-        <li><a href="#">Services</a></li>
         <li><a href="#">Contact</a></li>
+        <li><a href="#">Search</a></li>
       </ul>
     </nav>
   );
